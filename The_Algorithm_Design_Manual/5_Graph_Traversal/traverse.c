d396141d047f0a3ed08297070c1e4f02ad743055
Minjie Zha <minjiezha@gmail.com>
2013-09-12 09:43:30 +0800

    add traverse

---
+++ The_Algorithm_Design_Manual/5_Graph_Traversal/traverse.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include "traverse.h"
+#include "queue.h"
+
+static void run_bfs(Graph* g, int start, TraverseListener* listener, int* discovered, int* processed) {
+    Queue* q = newQueue(g->nvertices);
+    enqueue(q, start);
+    discovered[start] = 1;
+
+    while (!emptyQueue(q)) {
+        int v = dequeue(q);
+        listener->beforeProcess(start);
+
+        Node* node = g->nodes[v];
+        while (node != NULL) {
+            int y = node->label;
+            if (!discovered[y]) {
+                enqueue(q, y);
+                discovered[y] = 1;
+                listener->onDiscovered(y, v);
+            } else if (!processed[y] || g->directed) {
+                listener->onProcessEdge(v, y);
+            }
+
+            node = node->next;
+        }
+
+        processed[v] = 1;
+        listener->afterProcess(v);
+    }
+}
+
+void bfs(Graph* g, int start, TraverseListener* listener) {
+    int size = g->nvertices;
+
+    int* discovered = (int*) malloc((size+1) * sizeof(int));
+    int* processed = (int*) malloc((size+1) * sizeof(int));
+    int i;
+    for (i=0;i<size+1;i++) {
+        discovered[i] = 0;
+        processed[i] = 0;
+    }
+
+    run_bfs(g,start,listener,discovered,processed);
+
+    free(discovered);
+    free(processed);
+}
---
+++ The_Algorithm_Design_Manual/5_Graph_Traversal/traverse.h
@@ -0,0 +1,14 @@
+#ifndef TRAVERSE_H
+#define TRAVERSE_H
+#include "graph.h"
+
+typedef struct {
+    void (*beforeProcess) (int v);
+    void (*afterProcess) (int v);
+    void (*onDiscovered) (int t, int s);
+    void (*onProcessEdge) (int x, int y);
+} TraverseListener;
+
+void bfs(Graph* g, int start, TraverseListener* listener);
+
+#endif
